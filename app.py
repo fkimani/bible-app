8928381cfdf89685c10313e5d75ef0d59be528bb
fkimani <mainafk@gmail.com>
2022-09-13 01:17:17 -0400

    templates searchtext

--- app.py
+++ app.py
@@ -7,12 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def search():
-    print("Index page")
-    return render_template("index.html", results="this is it!")
+    print("Index page...")
+    return render_template("index.html")
 
-@app.route('/results', methods=['GET'])
+@app.route('/results', methods=['GET', 'POST'])
 def results():
-    print("results page")
-    return
+    print("Results page...")
+    searchtext = request.args.get('id')
+    print(f'searchtext: {searchtext}')
+    # db operations here with searchtext
+    return render_template("results.html", results=searchtext)
 
 
