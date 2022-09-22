a3bb9602c62cbcad57220f9f4ccd08cdf40de366
fkimani <mainafk@gmail.com>
2022-09-22 16:52:34 -0400

    can imporove appcode

--- app.py
+++ app.py
@@ -30,18 +30,13 @@ def bookname(id):
 #format bookname to bookid
 def book_id(book_name):
     bk_code = booklist.index(book_name)
-    bk_code +=1
-    if(int(bk_code) < 10):
-        bk_code = f'0{bk_code}'
-    return bk_code
+    bk_code +=1 # since array starts from 0
+    bk_code = f'{bk_code}'
+    return bk_code.zfill(2) #prepend zeros to have 2 digits
 # format chapter code to chapter
-def chapter_id(chapter):
-    if int(chapter)<10:
-        return f'00{chapter}'
-    if int(chapter)<100:
-        return f'0{chapter}'
-    else:
-        return chapter
+def chapter_id(chvs):
+    chvs = f'{chvs}'
+    return chvs.zfill(3) #prepend zeros to have 3 digits
 # get versions
 def get_versions():
     cur = db_conn()
@@ -57,7 +52,7 @@ def get_version_abbrev(vsn):
     print(f'Fetching "{vsn}" abbrev...')
     for a in versions:
         if vsn in a[1]:
-            print(f'{a[1]} = {a[2]} ')
+            print(f'Fetched. {a[1]} = {a[2]} ')
             abbrev = a[2]
     return abbrev
 def keyword_search(kw_input,bk_version):
@@ -89,13 +84,35 @@ def dropdown_search(id_input, bk_version):
         id_rslt = result.fetchone()
         id_result = f'{bookname(id_rslt[1])} {id_rslt[2]}:{id_rslt[3]}: {id_rslt[4]}'
     return id_result
-
+# convert code to bcv
+def get_bcv(id_input):
+    if len(id_input) != 8:
+        raise ValueError(f'"{id_input}" has incorrect length of {len(id_input)}')
+    bk_id = id_input[0:2]
+    ch_id = id_input[2:5]
+    vs_id = id_input[5:8]
+    bk_name = bookname(bk_id)
+    chapter = int(ch_id)
+    verse = int(vs_id)
+    bcv_format = f'{bk_name} {chapter}:{verse}'
+    print(f'Decode {id_input} to bcv format: {bcv_format}')
+    return bcv_format
+# convert searchtext to 8 digit code
+def get_bkcode(searchtext):
+    # convert vars to searchable 8 digit ids
+    r = f"{searchtext['bk']} {searchtext['ch']}:{searchtext['vs']}" 
+    bid = book_id(searchtext['bk'])
+    cid = chapter_id(searchtext['ch'])
+    vid = chapter_id(searchtext['vs'])
+    bcvid = f"{bid}{cid}{vid}" 
+    print(f'Encode {r} to {bcvid}')
+    return bcvid
 # search / index route
 @app.route('/')
 def search():
     print("Index page...")
     versions = get_versions()
-    print(f'versions: {versions}')
+    # print(f'versions: {versions}')
     versecount = 160
     chaptercount = 150
     booklist = []
@@ -115,12 +132,14 @@ def results():
     id_input = ''
     id_result = ''
     kw_len = ''
+    bcv=''
     bk_version = request.args.get('version')
     bk_version_nm = get_version_abbrev(bk_version)
     # 2. dropdown search
     bk_input = request.args.get('bk')
     ch_input = request.args.get('ch')
     vs_input = request.args.get('vs')
+    vs_end_input = request.args.get('vs_end')
     # advanced id search
     id_input = request.args.get('id')
 
@@ -131,19 +150,38 @@ def results():
     elif id_input != '':
         kw_len = 0
         searchtext = id_input 
+        bcv = get_bcv(searchtext)
         searchresult = dropdown_search(id_input,bk_version)
-        print(f' "{id_input}" search result: {searchresult}')
+        print(f' "{id_input}" search result: {searchresult}'
     else:
         #default is dropdown search
         kw_len = 0
-        searchtext = f'{bk_input} {ch_input}:{vs_input}'
-        print(f'searchtext: {searchtext}')
+        # TODO start changehere (=to func)
+        # searchtext = f'{bk_input} {ch_input}:{vs_input}'
+        searchtext = dict([('bk',bk_input), ('ch',ch_input), ('vs',vs_input)])
+        # print(bcvid)
+        # utilities.get_bkcode(bcvid)
+        print(f'searchtext in results page: {searchtext}')
         bk_id = book_id(bk_input)
         ch_id = chapter_id(ch_input)
         vs_id = chapter_id(vs_input)
-        searchtext_id = f'{bk_id}{ch_id}{vs_id}'
-        print(f'convert {searchtext} to searchtext_id code: {searchtext_id}')
-        searchresult = dropdown_search(searchtext_id,bk_version)
+        vs_end_id = chapter_id(vs_end_input)
+        # end changehere
+        if vs_end_id != '':
+            searchtext_id = f'{bk_id}{ch_id}{vs_id}'
+            searchtext_id2 = f'{bk_id}{ch_id}{vs_end_id}'
+            searchtext_id1 = get_bkcode(searchtext_id1)
+            searchtext_id2 = get_bkcode(searchtext_id2)
+            bcv1 = get_bcv(searchtext_id1)
+            bcv2 = get_bcv(searchtext_id2)
+            bcv = f'{bcv1}-{bcv2[5:7]}'
+            print(f'bcv for vs-vs_end: {bcv}')
+        else:
+            searchtext_id = f'{bk_id}{ch_id}{vs_id}'
+            searchtext_id = get_bkcode(searchtext)
+            bcv = get_bcv(searchtext_id)
+            print(f'convert {searchtext} to searchtext_id code: {searchtext_id}')
+            searchresult = dropdown_search(searchtext_id,bk_version)
 
-    return render_template("results.html", searchtext=searchtext, results=searchresult, booklist=booklist, kw_input=kw_input, kw_result=kw_result, kw_len=kw_len, id_input=id_input, id_result=id_result, bk_version=bk_version,bk_version_nm=bk_version_nm)
+    return render_template("results.html", searchtext=searchtext, results=searchresult, booklist=booklist, kw_input=kw_input, kw_result=kw_result, kw_len=kw_len, id_input=id_input, id_result=id_result, bk_version=bk_version,bk_version_nm=bk_version_nm, bcv=bcv)
     
